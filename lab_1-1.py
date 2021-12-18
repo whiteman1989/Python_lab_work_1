3561d47c0b8a0b21a0cb5cdc1df48500ac6a5866
AndreyGorban <Whiteman1989@gmail.com>
2021-12-18 11:45:53 +0200

    move input messages to input methods

--- lab_1-1.py
+++ lab_1-1.py
@@ -1,24 +1,11 @@
 # inputs
-print("Ваші прізвище, ім'я, по батькові? ", end='')
-ful_name = input()
-
-print("Скільки Вам років? ", end='')
-age = int(input())
-
-print("Де Ви живете? ", end='')
-adress = input()
-
-print("Де Ви навчаєтесь? ", end='')
-place_of_study = input()
-
-print("Номер Вашої групи? ", end='')
-group_number = int(input())
-
-print("Який Ваш порядковий номер у списку групи? ", end='')
-number_in_group = int(input())
-
-print("Як справи? ", end=''),
-how_are_you = input()
+ful_name = input("Ваші прізвище, ім'я, по батькові? ")
+age = int(input("Скільки Вам років? "))
+adress = input("Де Ви живете? ")
+place_of_study = input("Де Ви навчаєтесь? ")
+group_number = int(input("Номер Вашої групи? "))
+number_in_group = int(input("Який Ваш порядковий номер у списку групи? "))
+how_are_you = input("Як справи? ")
 
 #outputs
 print()
