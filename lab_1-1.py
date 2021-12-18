76cdb5f9bb914497a893d40e29e042751fd743e4
AndreyGorban <Whiteman1989@gmail.com>
2021-12-18 11:28:02 +0200

    first commit

---
+++ lab_1-1.py
@@ -0,0 +1,31 @@
+# inputs
+print("Ваші прізвище, ім'я, по батькові? ", end='')
+ful_name = input()
+
+print("Скільки Вам років? ", end='')
+age = int(input())
+
+print("Де Ви живете? ", end='')
+adress = input()
+
+print("Де Ви навчаєтесь? ", end='')
+place_of_study = input()
+
+print("Номер Вашої групи? ", end='')
+group_number = int(input())
+
+print("Який Ваш порядковий номер у списку групи? ", end='')
+number_in_group = int(input())
+
+print("Як справи? ", end=''),
+how_are_you = input()
+
+#outputs
+print()
+print("Ваше ім'я: ", ful_name)
+print("Ваш вік: ", age)
+print("Ви живете в: ", adress)
+print("Ви навчаєтесь в: ", place_of_study)
+print("Номер вашої групи: ", group_number)
+print("Ваш порядковий номер у списку групи: ", number_in_group)
+print("Ваші спрви: ", how_are_you)
